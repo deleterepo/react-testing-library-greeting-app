a80ec7386d8092e87ba2d77bd6022511bd4a7a3a
Jordan <jordansherman@live.ca>
2019-12-21 11:55:41 -0500

    Installed axios. Added unit test for greeting. Mocking axios api call using jest.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import axiosMock from 'axios'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('axios')
+
+test('loads and displays greeting', async () => {
+  const url = '/greeting'
+  const { getByText, getByRole } = render(<App url={url} />)
+
+  axiosMock.get.mockResolvedValueOnce({
+    data: { greeting: 'hello there' },
+  })
+
+  fireEvent.click(getByText('Load Greeting'))
+
+  const greetingTextNode = await waitForElement(() => getByRole('heading'))
+
+  expect(axiosMock.get).toHaveBeenCalledTimes(1)
+  expect(axiosMock.get).toHaveBeenCalledWith(url)
+  expect(getByRole('heading')).toHaveTextContent('hello there')
+  expect(getByRole('button')).toHaveAttribute('disabled')
+})
