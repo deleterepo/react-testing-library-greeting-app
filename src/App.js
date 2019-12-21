046b5b1fccc8e0a56bc369d31fa44368a50769be
Jordan <jordansherman@live.ca>
2019-12-21 12:02:40 -0500

    Updating implementation  of component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
+export default function Fetch({ url }) {
+  const [greeting, setGreeting] = useState('')
+  const [buttonClicked, setButtonClicked] = useState(false)
+
+  const fetchGreeting = async () => {
+    const response = await axios.get(url)
+    const data = response.data
+    const { greeting } = data
+    setGreeting(greeting)
+    setButtonClicked(true)
+  }
+
+  const buttonText = buttonClicked ? 'Ok' : 'Load Greeting'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchGreeting} disabled={buttonClicked}>
+        {buttonText}
+      </button>
+      {greeting ? <h1>{greeting}</h1> : null}
     </div>
-  );
+  )
 }
-
-export default App;
